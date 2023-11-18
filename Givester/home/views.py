8be96aea2ffe8fb14bda5f9b5efe8f08fac117e2
Jeff <jeffreywjlim@gmail.com>
2023-11-18 10:57:22 -0500

    pain

--- Givester/home/views.py
+++ Givester/home/views.py
@@ -5,3 +5,7 @@ from django.http import HttpResponse
 def home(request):
     #return HttpResponse("Hello, world. You're at the polls index.")
     return render(request, "home.html")
+
+def about(request):
+    #return HttpResponse("Hello, world. You're at the polls index.")
+    return render(request, "about.html")
