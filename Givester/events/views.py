23cfb1ea64bd49da9ef0e0bb93be4106951b78d5
Jacob Lau <jacoblau05@gmail.com>
2023-11-19 10:42:54 -0500

    changed date of wash dishes and prepped for merge

--- Givester/events/views.py
+++ Givester/events/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render, HttpResponse
 from .models import Event
+from django.utils.timezone import datetime
 
 # Create your views here.
 def events(request):
     #return HttpResponse("Hello, world. You're at the polls index.")
     items = Event.objects.all()
-    return render(request, "events.html", {'event': items })
+    date = datetime.date(datetime.now())
+    return render(request, "events.html", {'event': items , 'date': date})
 
 def apply(request,data):
     #return HttpResponse("Hello, world. You're at the polls index.")
